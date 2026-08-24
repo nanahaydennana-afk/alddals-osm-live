d26df24aacb0f538327890660f8f11e5b70d3aea
nanahaydennana-afk <nana.hayden.nana@gmail.com>
2026-08-25 08:10:40 +0900

    Deploy Achasan Tongdak, The Nom, and Doldam listings

---
+++ stores-extra.js
@@ -0,0 +1,225 @@
+// GitHub 기준 매장 데이터에 이어 붙이는 운영 추가분입니다.
+bars.push({
+  id:29,
+  name:'무지개맥주 가좌점',
+  district:'서울시',
+  hood:'서대문구',
+  area:'서울 서대문구 수색로2길 39 상가동 1층 108호 · 가좌역 인근',
+  soju:8000,
+  sojuLabel:'무제한 8,000원',
+  beer:8000,
+  beerLabel:'무제한 8,000원',
+  food:NaN,
+  foodLabel:'안주 별도 주문',
+  total:8000,
+  totalLabel:'주류 1인 8,000원',
+  checked:'2026.08.20',
+  eventChecked:'2026.08.20',
+  deal:'해피아워 · 1인 8,000원 · 소주·맥주 2시간 무제한 · 방문 전 적용 조건 확인 권장',
+  lat:37.579564,
+  lng:126.9113101,
+  source:'https://map.naver.com/p/entry/place/1407881152?lng=126.9113101&lat=37.579564&placePath=%2Fhome%3Ffrom%3Dmap%26fromPanelNum%3D1%26additionalHeight%3D76%26timestamp%3D202608201648%26locale%3Dko%26svcName%3Dmap_pcv5&entry=plt&searchType=place&c=14.28,0,0,0,dh',
+  directions:'https://map.naver.com/p/directions/-/126.9113101,37.579564/%EB%AC%B4%EC%A7%80%EA%B0%9C%EB%A7%A5%EC%A3%BC%20%EA%B0%80%EC%A2%8C%EC%A0%90/-/transit',
+  images:[
+    'assets/mujigae-beer-gajwa/platter.webp',
+    'assets/mujigae-beer-gajwa/unlimited-event.webp',
+    'assets/mujigae-beer-gajwa/chicken-soup.webp'
+  ]
+});
+
+bars.push({
+  id:30,
+  name:'순백회관 영등포구청역점',
+  district:'서울시',
+  hood:'영등포구',
+  area:'서울 영등포구 당산로 90 호서빌딩 1층 106호 · 영등포구청역 인근',
+  soju:1900,
+  beer:NaN,
+  food:9000,
+  foodLabel:'순대국밥 9,000원',
+  total:10900,
+  totalLabel:'국밥+소주 10,900원',
+  checked:'2026.08.20',
+  eventChecked:'2026.08.20',
+  deal:'여름 한정 이벤트 · 순대국밥 9,000원 · 소주 1,900원 · 방문 전 종료 여부 확인 권장',
+  lat:37.5227561,
+  lng:126.8961878,
+  source:'https://map.naver.com/p/entry/place/2028708585?c=14.28,0,0,0,dh&placePath=%2Fphoto%3Ffrom%3Dmap%26fromPanelNum%3D1%26additionalHeight%3D76%26timestamp%3D202608201716%26locale%3Dko%26svcName%3Dmap_pcv5',
+  directions:'https://map.naver.com/p/directions/-/126.8961878,37.5227561/%EC%88%9C%EB%B0%B1%ED%9A%8C%EA%B4%80%20%EC%98%81%EB%93%B1%ED%8F%AC%EA%B5%AC%EC%B2%AD%EC%97%AD%EC%A0%90/-/transit',
+  images:[
+    'assets/sunbaekhall-yeongdeungpo/menu-spread.webp',
+    'assets/sunbaekhall-yeongdeungpo/summer-event.webp'
+  ]
+});
+
+bars.push({
+  id:31,
+  name:'크치치킨 회기점',
+  district:'서울시',
+  hood:'동대문구',
+  area:'서울 동대문구 회기로 125 1층 · 회기역·경희대 인근',
+  soju:NaN,
+  beer:10000,
+  beerLabel:'8종 무제한 10,000원',
+  food:NaN,
+  foodLabel:'치킨 주문 필수',
+  total:10000,
+  totalLabel:'1인 10,000원+치킨',
+  checked:'2026.08.20',
+  eventChecked:'2026.08.20',
+  deal:'생맥주 8종 3시간 무제한 · 1인 10,000원 · 2인당 치킨 1개 주문 · 크치치킨 인스타그램 팔로우 필수 · 직원에게 이벤트 참여 요청',
+  lat:37.5915773,
+  lng:127.0505318,
+  source:'https://map.naver.com/p/entry/place/754792240?lng=127.0505318&lat=37.5915773&placePath=%2Fphoto%3Ffrom%3Dmap%26fromPanelNum%3D1%26additionalHeight%3D76%26timestamp%3D202608202130%26locale%3Dko%26svcName%3Dmap_pcv5%26filterType%3DAI%20View%26subFilter%3DFOOD&entry=plt&searchType=place&c=15.00,0,0,0,dh',
+  directions:'https://map.naver.com/p/directions/-/127.0505318,37.5915773/%ED%81%AC%EC%B9%98%EC%B9%98%ED%82%A8%20%ED%9A%8C%EA%B8%B0%EC%A0%90/-/transit',
+  images:[
+    'assets/cchi-chicken-hoegi/chicken-platter.webp',
+    'assets/cchi-chicken-hoegi/beer-event.webp',
+    'assets/cchi-chicken-hoegi/interior.webp'
+  ]
+});
+
+bars.push({
+  id:32,
+  name:'루프탑 셀프포차',
+  district:'서울시',
+  hood:'관악구',
+  area:'서울 관악구 신림로59길 15-10 삼보빌딩 4층 · 신림역 3번 출구 인근',
+  soju:3500,
+  beer:4500,
+  beerLabel:'병맥주·크림생맥주 4,500원',
+  food:NaN,
+  foodLabel:'안주 별도 주문',
+  total:3500,
+  totalLabel:'주류 3,500원부터',
+  checked:'2026.08.22',
+  eventChecked:'2026.08.22',
+  deal:'셀프바 · 소주 3,500원 · 병맥주·크림생맥주 4,500원 · 음료 2,000원 · 냉장고에서 직접 가져오는 방식 · 루프탑·대형 단체석',
+  lat:37.482765723807,
+  lng:126.92918130044,
+  source:'https://map.naver.com/p/entry/place/1049033446?c=15.00,0,0,0,dh&placePath=%2Fmenu%3FfromPanelNum%3D1%26additionalHeight%3D76%26timestamp%3D202608220943%26locale%3Dko%26svcName%3Dmap_pcv5%26filterType%3D%EC%97%85%EC%B2%B4',
+  directions:'https://map.naver.com/p/directions/-/126.92918130044,37.482765723807/%EB%A3%A8%ED%94%84%ED%83%91%20%EC%85%80%ED%94%84%ED%8F%AC%EC%B0%A8/-/transit',
+  images:[
+    'assets/rooftop-self-pocha/menu.webp',
+    'assets/rooftop-self-pocha/table.webp',
+    'assets/rooftop-self-pocha/venue.webp'
+  ]
+});
+
+bars.push({
+  id:33,
+  name:'소다만식품',
+  district:'서울시',
+  hood:'종로구',
+  area:'서울 종로구 수표로 92 1층 · 종로3가역 15번 출구 인근',
+  soju:3500,
+  beer:3500,
+  food:10000,
+  foodLabel:'메인메뉴 10,000원부터',
+  total:13500,
+  totalLabel:'메인+주류 13,500원부터',
+  checked:'2026.08.22',
+  eventChecked:'2026.08.22',
+  deal:'가맥집 · 소주·맥주·막걸리 각 3,500원 · 메인메뉴 테이블당 필수 · 이용시간 2시간 이내 · 모든 과자 2,500원 · 방문 전 가격 확인 권장',
+  lat:37.569472613836,
+  lng:126.9895763371,
+  source:'https://map.naver.com/p/search/%EC%86%8C%EB%8B%A4%EB%A7%8C%EC%8B%9D%ED%92%88?c=15.00,0,0,0,dh&placePath=%2Fhome%3FfromPanelNum%3D1%26additionalHeight%3D76%26timestamp%3D202608220947%26locale%3Dko%26svcName%3Dmap_pcv5%26searchText%3D%EC%86%8C%EB%8B%A4%EB%A7%8C%EC%8B%9D%ED%92%88%26filterType%3D%EC%97%85%EC%B2%B4',
+  directions:'https://map.naver.com/p/directions/-/126.9895763371,37.569472613836/%EC%86%8C%EB%8B%A4%EB%A7%8C%EC%8B%9D%ED%92%88/-/transit',
+  images:[
+    'assets/sodaman-foods-jongno/egg-roll.webp',
+    'assets/sodaman-foods-jongno/storefront.webp',
+    'assets/sodaman-foods-jongno/menu.webp'
+  ]
+});
+
+bars.push({
+  id:34,
+  name:'아차산통닭',
+  district:'서울시',
+  hood:'광진구',
+  area:'서울 광진구 천호대로 659 1층 · 아차산역 1번 출구 인근',
+  soju:3500,
+  beer:3500,
+  beerPinTitle:'생맥주',
+  beerLabel:'생맥주 3,500원',
+  food:5000,
+  foodLabel:'옛날통닭 5,000원',
+  total:8500,
+  totalLabel:'통닭+소주 8,500원',
+  checked:'2026.08.24',
+  eventChecked:'2026.08.24',
+  deal:'24시간 연중무휴 · 오픈기념 막걸리 2,500원 · 소주 3,500원 · 생맥주 3,500원 · 옛날통닭 5,000원 · 방문 전 가격 변동 확인 권장',
+  lat:37.552303850246,
+  lng:127.08993508113,
+  source:'https://map.naver.com/p/entry/place/1630172645',
+  directions:'https://map.naver.com/p/directions/-/127.08993508113,37.552303850246/%EC%95%84%EC%B0%A8%EC%82%B0%ED%86%B5%EB%8B%AD/-/transit',
+  images:[
+    'assets/achasan-tongdak/chicken-mandu-gizzard.webp',
+    'assets/achasan-tongdak/storefront.webp',
+    'assets/achasan-tongdak/gizzard-salad.webp'
+  ]
+});
+
+bars.push({
+  id:35,
+  name:'더놈 THE NOM',
+  district:'서울시',
+  hood:'서초구',
+  area:'서울 서초구 방배로 83-9 동화빌딩 지하 1층 · 방배동 · 방배역 3번 출구 인근',
+  soju:20000,
+  sojuTitle:'칵테일 무제한 · 여성',
+  sojuPinTitle:'여성',
+  beer:25000,
+  beerTitle:'칵테일 무제한 · 남성',
+  beerPinTitle:'남성',
+  food:NaN,
+  foodTitle:'이용 방식',
+  foodLabel:'칵테일 무제한',
+  total:NaN,
+  totalTitle:'영업시간',
+  totalLabel:'19:00~04:00',
+  checked:'2026.08.24',
+  eventChecked:'2026.08.24',
+  deal:'칵테일 무제한 · 여성 20,000원 · 남성 25,000원 · 90여 종 칵테일 안내 · 매일 19:00~04:00 · 방문 전 가격과 운영 여부 확인 권장',
+  lat:37.4820109,
+  lng:126.9966397,
+  source:'https://map.naver.com/p/search/%EB%B0%A9%EB%B0%B0%EB%8F%99%20%EB%8D%94%EB%86%88?c=15.00,0,0,0,dh&placePath=%2Fmenu%3FfromPanelNum%3D1%26additionalHeight%3D76%26timestamp%3D202608241758%26locale%3Dko%26svcName%3Dmap_pcv5%26searchText%3D%EB%B0%A9%EB%B0%B0%EB%8F%99%20%EB%8D%94%EB%86%88',
+  directions:'https://map.naver.com/p/directions/-/126.9966397,37.4820109/%EB%8D%94%EB%86%88%20THE%20NOM/-/transit',
+  images:[
+    'assets/the-nom-bangbae/dessert-cocktail.webp',
+    'assets/the-nom-bangbae/unlimited-menu.webp',
+    'assets/the-nom-bangbae/bar-interior.webp'
+  ]
+});
+
+bars.push({
+  id:36,
+  name:'돌담',
+  district:'서울시',
+  hood:'동작구',
+  area:'서울 동작구 동작대로27다길 12 2층 · 사당동 · 이수역 인근',
+  soju:25000,
+  sojuTitle:'칵테일 무제한',
+  sojuPinTitle:'무제한',
+  beer:23000,
+  beerTitle:'현금 결제 안내',
+  beerPinTitle:'현금가',
+  food:NaN,
+  foodTitle:'이용 대상',
+  foodLabel:'남녀 동일가',
+  total:NaN,
+  totalTitle:'영업시간',
+  totalLabel:'19:00~01:00',
+  checked:'2026.08.24',
+  eventChecked:'2026.08.24',
+  deal:'칵테일 무제한 · 남녀 1인 25,000원 · 현금 결제 시 23,000원 안내 · 논알콜과 다양한 도수 선택 가능 · 방문 전 가격과 운영 여부 확인 권장',
+  lat:37.4863,
+  lng:126.9828,
+  source:'https://map.naver.com/p/search/%EC%82%AC%EB%8B%B9%20%EB%8F%8C%EB%8B%B4?c=15.00,0,0,0,dh&placePath=%2Fphoto%3FfromPanelNum%3D1%26additionalHeight%3D76%26timestamp%3D202608241804%26locale%3Dko%26svcName%3Dmap_pcv5%26searchText%3D%EC%82%AC%EB%8B%B9%20%EB%8F%8C%EB%8B%B4',
+  directions:'https://map.naver.com/p/directions/-/126.9828,37.4863/%EB%8F%8C%EB%8B%B4/-/transit',
+  images:[
+    'assets/doldam-isu/cocktail.webp',
+    'assets/doldam-isu/bar-interior.webp',
+    'assets/doldam-isu/unlimited-menu.webp'
+  ]
+});
